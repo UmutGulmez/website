90d2ac5824b6cb35abae5737047b3d60a0c5e0bd
UmutGulmez <2005umututku@gmail.com>
2021-05-30 17:47:57 +0300

    new feature: add google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -106,7 +106,7 @@ export default {
   css: ['~/assets/style/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['@/plugins/googleAnalytics'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
---
+++ plugins/googleAnalytics.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import VueGtag from 'vue-gtag'
+
+Vue.use(VueGtag, {
+  config: { id: 'G-8GFWSVEEF8' }, // Enter "your" google analytics tracikng code.
+})
